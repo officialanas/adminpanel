bf5dd82fd2ec32d793ff3f7f92b2d007c0d38614
Anas Ansari <itsofficial177@gmail.com>
2023-04-07 15:57:57 +0530

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./home.scss";
+import Sidebar from "../../component/sidebar/Sidebar";
+import Navbar from "../../component/navbar/Navbar";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        home container
+      </div>
+    </div>
+  );
 };
 
 export default Home;
